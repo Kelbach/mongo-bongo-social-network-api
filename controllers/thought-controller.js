2e874be2333c89d9bc004620ae56936f11a3839d
kelbach <87092340+Kelbach@users.noreply.github.com>
2021-12-27 12:14:33 -0500

    controllers setup

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { User, Thoughts } = require('../models');
+
+const thoughtController = {
+    //getAllThoughts
+
+    //getThoughtById
+
+    //createThought
+
+    //updateThought
+
+    //deleteThought
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,17 @@
+const { User, Thoughts } = require('../models');
+
+const userController = {
+    //getAllUsers
+
+    //getUserbyId
+
+    //createUser
+
+    //updateUser
+
+    //deleteUser
+    //bonus: delete user's associated thoughts
+    
+};
+
+module.exports = userController;
